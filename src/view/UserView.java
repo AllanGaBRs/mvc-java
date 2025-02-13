fd3033779654ecbad2e75455d3e81a7e0185c8af
allangabr <allanmediogames@gmail.com>
2025-02-13 16:13:09 -0300

    UserView: created method showMessage

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -3,14 +3,18 @@ package view;
 import model.entities.Address;
 
 public class UserView {
-	
+
 	public void showUserDetails(String name, int age, String email, Address address, String cpf) {
-        System.out.println("\nUsuário encontrado:");
-        System.out.println("Nome do usuario: " + name);
-        System.out.println("Idade do " + name + ": " + age);
-        System.out.println("E-mail do " + name + ": " + email);
-        System.out.println("Endereço do " + name + ": " + address.toString());
-        System.out.println("CPF do " + name + ": " + cpf);
-    }
-	
+		System.out.println("\nUsuário encontrado:");
+		System.out.println("Nome do usuario: " + name);
+		System.out.println("Idade do " + name + ": " + age);
+		System.out.println("E-mail do " + name + ": " + email);
+		System.out.println("Endereço do " + name + ": " + address.toString());
+		System.out.println("CPF do " + name + ": " + cpf);
+	}
+
+	public void showMessage(String message) {
+		System.out.println(message);
+	}
+
 }
