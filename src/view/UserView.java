877cecc860fd2f463fa9a377cd7c45917870645a
allangabr <allanmediogames@gmail.com>
2025-02-13 16:12:23 -0300

    UserView: created method showUserDetails

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -1,5 +1,16 @@
 package view;
 
-public class UserView {
+import model.entities.Address;
 
+public class UserView {
+	
+	public void showUserDetails(String name, int age, String email, Address address, String cpf) {
+        System.out.println("\nUsuário encontrado:");
+        System.out.println("Nome do usuario: " + name);
+        System.out.println("Idade do " + name + ": " + age);
+        System.out.println("E-mail do " + name + ": " + email);
+        System.out.println("Endereço do " + name + ": " + address.toString());
+        System.out.println("CPF do " + name + ": " + cpf);
+    }
+	
 }
