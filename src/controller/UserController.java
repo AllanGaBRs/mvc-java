bd97871b1c3e82cf4c8f86caf09a45e2e8dc10ba
allangabr <allanmediogames@gmail.com>
2025-02-13 16:16:10 -0300

    UserController: created method addUser

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,5 +1,43 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.entities.Address;
+import model.entities.User;
+import view.UserView;
+
 public class UserController {
 
+	private ArrayList<User> users;
+	private UserView view;
+
+	public UserController(UserView view) {
+		this.users = new ArrayList<>();
+		this.view = view;
+	}
+
+	public void addUser(String name, int age, String email, String street, Integer number, String cpf) {
+		for (User user : users) {
+			if (user.getCpf().equals(cpf)) {
+				view.showMessage("Erro: O CPF '" + cpf + "' já está cadastrado.");
+				return;
+			}
+		}
+
+		try {
+			Address newAddress = new Address();
+			newAddress.setStreet(street);
+			newAddress.setNumber(number);
+			User newUser = new User();
+			newUser.setName(name);
+			newUser.setAge(age);
+			newUser.setCpf(cpf);
+			newUser.setAddress(newAddress);
+			newUser.setEmail(email);
+			users.add(newUser);
+			view.showMessage("Usuário " + newUser.getName() + " cadastrado com sucesso!");
+		} catch (IllegalArgumentException e) {
+			view.showMessage("Erro ao adicionar usuário: " + e.getMessage());
+		}
+	}
 }
