0bd93d21f07615a455916579c24c7c2f9b81f446
allangabr <allanmediogames@gmail.com>
2025-02-13 16:05:20 -0300

    entities: user: validation in setAge

--- src/model/entities/User.java
+++ src/model/entities/User.java
@@ -1,18 +1,18 @@
 package model.entities;
 
 public class User {
-	
-    private String name;
-    private int age;
-    private String email;
-    private Address address;
-    private String cpf;
 
-    public User() {
-    	
-    }
-        
-    public User(String name, int age, String email, String street, Integer number, String cpf) {
+	private String name;
+	private int age;
+	private String email;
+	private Address address;
+	private String cpf;
+
+	public User() {
+
+	}
+
+	public User(String name, int age, String email, String street, Integer number, String cpf) {
 		this.name = name.trim();
 		this.age = age;
 		this.email = email.trim();
@@ -21,56 +21,57 @@ public class User {
 	}
 
 	public String getName() {
-        return name;
-    }
+		return name;
+	}
 
 	public void setName(String name) {
-		 if (name == null || name.isBlank()) {
-		        throw new IllegalArgumentException("Nome não pode ser vazio");
-		    }
-		    if(name.trim().length() < 3) {
-		    	throw new IllegalArgumentException("Nome deve conter no min 3 caracteres");
-		    }
-	    this.name = name;
+		if (name == null || name.isBlank()) {
+			throw new IllegalArgumentException("Nome não pode ser vazio");
+		}
+		if (name.trim().length() < 3) {
+			throw new IllegalArgumentException("Nome deve conter no min 3 caracteres");
+		}
+		this.name = name;
 	}
 
+	public int getAge() {
+		return age;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public void setAge(int age) {
+		if (age <= 0 || age > 120) {
+			throw new IllegalArgumentException("Idade inválida");
+		}
+		this.age = age;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setEmail(String email) {
+		this.email = email.trim();
+	}
 
-    public void setEmail(String email) {
-        this.email = email.trim();
-    }
+	public Address getAddress() {
+		return address;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
+	public void setAddress(Address address) {
 		this.address = address;
 	}
-    
-    public String getCpf() {
-        return cpf;
-    }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf.trim();
-    }
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf.trim();
+	}
 
 	@Override
 	public String toString() {
 		return name;
 	}
-    
-    
+
 }
